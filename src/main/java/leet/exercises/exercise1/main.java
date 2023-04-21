ef3172ee63f726f63020b05c81a57a7b340352b1
Michelle Trejo <michelletgar@gmail.com>
2023-04-20 22:00:33 -0600

    first exercise

--- src/main/java/leet/exercises/exercise1/main.java
+++ src/main/java/leet/exercises/exercise1/main.java
@@ -2,5 +2,11 @@ package leet.exercises.exercise1;
 
 public class main {
     public static void main(String[] args) {
+
+        TwoSum sum = new TwoSum();
+        int[] nums = {2,7,11,15};
+        int target = 9;
+
+        System.out.println(sum.twoSum(nums,target));
     }
 }
