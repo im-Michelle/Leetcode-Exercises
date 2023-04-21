6e109799c837a23ce4e3779b226dd0bdeeb37a08
JorgeTheroux <jorge.theroux@icloud.com>
2023-04-20 22:01:43 -0600

    vamos!

--- src/main/java/leet/exercises/exercise1/TwoSum.java
+++ src/main/java/leet/exercises/exercise1/TwoSum.java
@@ -2,4 +2,17 @@ package leet.exercises.exercise1;
 
 public class TwoSum {
 
+    public int[] sum(int[] patito23, int target){
+        for (int i = 0; i < patito23.length; i++) {
+            for (int j = 0; j < patito23.length; j++) {
+                if (patito23[i] + patito23[j] == target){
+                    return new int[] {i, j};
+                }
+                
+            }
+            
+        }
+        return new int[] {};
+    }
+
 }
--- src/main/java/leet/exercises/exercise1/main.java
+++ src/main/java/leet/exercises/exercise1/main.java
@@ -2,5 +2,11 @@ package leet.exercises.exercise1;
 
 public class main {
     public static void main(String[] args) {
+
+        TwoSum sumame = new TwoSum();
+        int array [] = {2,7,11,15 };
+
+        System.out.println(sumame.sum(array ,9));
+
     }
 }
